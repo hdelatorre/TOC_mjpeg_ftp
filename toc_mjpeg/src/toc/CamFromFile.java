8d61872a776a07a868874fc0505a8bef780bca27
Kuba <gajzler.jakub@gmail.com>
2012-12-11 17:20:18 +0000

    Loding list of Url from file finised

---
+++ toc_mjpeg/src/toc/CamFromFile.java
@@ -0,0 +1,47 @@
+package toc;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+
+public class CamFromFile {
+	
+	static final Logger logger = Logger.getLogger(CamFromFile.class);
+	
+	private String prefix = "http://";
+	//Camera needs to be setup to stream mjpeg on stream3 
+	private String suffix = "/video3.mjpg";
+	private BufferedReader bf;
+	private List<String> camList;
+	
+	public CamFromFile(String file){
+		BasicConfigurator.configure();
+		camList = new ArrayList<String>();
+		try{
+			logger.warn("Loading a camera list file");
+			
+			bf = new BufferedReader(new FileReader(file));
+			String line;
+			while((line = bf.readLine()) != null){
+				camList.add(prefix + line + suffix);
+			}
+			logger.warn("Loading cameras urls from a file finished");
+		}catch (FileNotFoundException e) {
+			
+			logger.warn(e.toString());
+		}catch (IOException e) {
+			
+			logger.warn(e.toString());
+		}
+	}
+	
+	public List getCamList(){
+		return camList;
+	}
+}
--- toc_mjpeg/src/toc/Main.java
+++ toc_mjpeg/src/toc/Main.java
@@ -1,8 +1,16 @@
 package toc;
 
+import java.util.ArrayList;
+
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("This is just a test");
+		CamFromFile cmf = new CamFromFile("camera_addr.txt");
+		ArrayList<String> list = (ArrayList) cmf.getCamList();
+		
+		for(String s : list){
+			System.out.println(s);
+		}
 	}
 
 }
