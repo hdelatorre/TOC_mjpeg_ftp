9bf61985bac201f7260c056968089033e35a4010
Kuba <gajzler.jakub@gmail.com>
2012-12-11 17:37:59 +0000

    Singleton

--- toc_mjpeg/src/toc/Settings.java
+++ toc_mjpeg/src/toc/Settings.java
@@ -1,5 +1,14 @@
 package toc;
 
 public class Settings {
-
+	private static Settings instance = null;
+	
+	protected Settings(){}
+	
+	public static Settings getInstance(){
+		if(instance == null){
+			instance = new Settings();
+		}
+		return instance;
+	}
 }
